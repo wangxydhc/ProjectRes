11b6c01fd3f484d29618efd10a61782102fff172
wangxydhc <wangxydhc@163.com>
2017-05-17 15:55:37 +0800

    add random get

--- WordRecite/src/action/ShowWords.java
+++ WordRecite/src/action/ShowWords.java
@@ -1,10 +1,14 @@
 package action;
+
+import java.util.Random;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import model.Word;
 import test.TestClass;
 import tools.Factory;
+
 public class ShowWords {
 	private int nums;
 
@@ -15,29 +19,33 @@ public class ShowWords {
 	public void setNums(int nums) {
 		this.nums = nums;
 	}
-	public void start(){
+
+	public void start() {
 		Factory.beforeclass();
-		Session s=Factory.getSessionFactory().openSession();
-	Transaction tx=s.beginTransaction();
-	Word [] res=new Word[nums];
-	for(int i=0;i<nums;i++)
-		res[i]=(Word)s.get(Word.class, i++);
-	tx.commit();
-	
-	for(int i=0;i<nums;i++){
-		if (res[i]!=null)
-		System.out.println(i+" "+res[i].getWord());
-		else
-			System.out.println(i);
-	}
-	s.close();
+		Session s = Factory.getSessionFactory().openSession();
+		Transaction tx = s.beginTransaction();
+		Word[] res = new Word[nums];
+		Random rd = new Random();
+		int id = rd.nextInt(2000)+1;
+		int i = 0;
+		while (nums-- > 0) {
+			res[i] = (Word) s.get(Word.class, id);
+			if (res[i] != null)
+				System.out.println("id: " + id + "      i: " + i
+						+ "      word:  " + res[i].getWord()
+						+ "           pre: " + res[i].getParaphrase());
+			else
+				System.out.println("&&&&&&&&&&&&&&&&"+id);
+			id = rd.nextInt(2000);
+			i++;
+		}
+		tx.commit();
+		s.close();
 	}
 
-	public static void main(String [] args){
-		ShowWords sw=new ShowWords();
-		sw.setNums(39);
-		TestClass tc=new TestClass();
-		
+	public static void main(String[] args) {
+		ShowWords sw = new ShowWords();
+		sw.setNums(500);
 		sw.start();
 	}
 }
